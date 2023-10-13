e630cce8436a40d06e563dc994085de9c2780efc
ryugahee <gagml910@hanmail.net>
2023-10-13 16:50:36 +0900

    .

---
+++ firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -0,0 +1,33 @@
+package com.example.firstproject.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Article {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column
+    private String title;
+    @Column
+    private String content;
+
+    public Article(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public Article() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
