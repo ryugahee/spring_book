44d6e828d8732a2b727613c51278622a320032e9
ryugahee <gagml910@hanmail.net>
2023-10-16 00:27:06 +0900

    Read

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -3,11 +3,19 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
 @Controller
 public class ArticleController {
 
@@ -19,15 +27,40 @@ public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
 
+    // 등록
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
-        System.out.println(form.toString());
-
+        log.info(form.toString());
+        //1. DTO를 엔티티로 변환
         Article article = form.toEntity();
-        System.out.println(article.toString());
-
+        log.info(article.toString());
+        //2. 레포지토리로 엔티티를 DB에 저장
         Article saved = articleRepository.save(article);
-        System.out.println(saved.toString());
-        return "";
+        log.info(saved.toString());
+
+        return "redirect:/articles/" + saved.getId();
+    }
+
+    // 조회
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        log.info("id = " + id);
+        //1. id를 조회해 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        //2. 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+        //3. 뷰 페이지 반환하기
+        return "articles/show";
+    }
+
+    //목록 조회
+    @GetMapping("/articles")
+    public String index(Model model) {
+        //1. 모든 데이터를 가져오기
+        ArrayList<Article> articleEntityList = articleRepository.findAll();
+        //2. 모델에 데이터 등록하기
+        model.addAttribute("articleList", articleEntityList);
+        //3. 뷰 페이지 설정하기
+        return "articles/index";
     }
 }
--- firstproject/src/main/java/com/example/firstproject/entity/Article.java
+++ firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -1,7 +1,15 @@
 package com.example.firstproject.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
 @Entity
 public class Article {
     @Id
@@ -12,22 +20,4 @@ public class Article {
     @Column
     private String content;
 
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    public Article() {
-
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
