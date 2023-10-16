199ef525ff84b5e8abc9f0483fb2b2cefacf4343
ryugahee <gagml910@hanmail.net>
2023-10-16 16:44:34 +0900

    REST API

--- firstproject/src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ firstproject/src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -7,11 +7,14 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 public class ArticleForm {
+    private Long id;
     private String title;
     private String content;
 
 
+    //DTO를 엔티티로 변환
     public Article toEntity() {
-        return new Article(null, title, content);
+        return new Article(id, title, content);
     }
+
 }
--- firstproject/src/main/java/com/example/firstproject/entity/Article.java
+++ firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -13,11 +13,17 @@ import lombok.ToString;
 @Entity
 public class Article {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column
     private String title;
     @Column
     private String content;
 
+    public void patch(Article article) {
+        if (article.title != null)
+            this.title = article.title;
+        if (article.content != null)
+            this.content = article.content;
+    }
 }
